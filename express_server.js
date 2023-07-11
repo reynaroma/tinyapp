b86246780f3fdf05d98e5b608e5db7270c07a27e
Reyna May <roma.reynamayA@gmail.com>
2023-07-11 15:30:15 -0600

    Add delete route for the specific ID

--- express_server.js
+++ express_server.js
@@ -54,6 +54,11 @@ app.get('/u/:id', (req, res) => {
   }
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect('/urls');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
