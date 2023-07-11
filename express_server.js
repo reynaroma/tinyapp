8233e23d45584feaaf1abd0d8020aff6979ffecb
Reyna May <roma.reynamayA@gmail.com>
2023-07-10 21:54:30 -0600

    Validate the redirection of the tiny app when it redirects to the short urls

--- express_server.js
+++ express_server.js
@@ -45,6 +45,15 @@ app.get('/urls/:id', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+app.get('/u/:id', (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.status(404).send('URL Not Found!');
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
